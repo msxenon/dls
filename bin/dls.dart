07d9886bc7e465a0f24ee2b4a7b1ea34470a2b4c
Modi <10958931+msxenon@users.noreply.github.com>
2025-08-20 09:00:45 +0200

    Add initial implementation of Dart Log Server with argument parsing

---
+++ bin/dls.dart
@@ -0,0 +1,51 @@
+import 'package:args/args.dart';
+import 'package:dls/src/dart_log_server.dart';
+
+const String dlsVersion = '0.0.1';
+
+ArgParser buildParser() {
+  return ArgParser()
+    ..addOption('port', abbr: 'p', defaultsTo: '8080', help: 'Port to run the Dart Log Server on.')
+    ..addFlag('help', abbr: 'h', negatable: false, help: 'Print this usage information.')
+    ..addFlag('verbose', abbr: 'v', negatable: false, help: 'Show additional command output.')
+    ..addFlag('version', negatable: false, help: 'Print the tool version.');
+}
+
+void printUsage(ArgParser argParser) {
+  print('Usage: dart dls.dart <flags> [arguments]');
+  print(argParser.usage);
+}
+
+void main(List<String> arguments) async {
+  final ArgParser argParser = buildParser();
+  try {
+    final ArgResults results = argParser.parse(arguments);
+    bool verbose = false;
+
+    // Process the parsed arguments.
+    if (results.flag('help')) {
+      printUsage(argParser);
+      return;
+    }
+    if (results.flag('version')) {
+      print('dls version: $dlsVersion');
+      return;
+    }
+    if (results.flag('verbose')) {
+      verbose = true;
+    }
+
+    // Act on the arguments provided.
+    print('Positional arguments: ${results.rest}');
+    if (verbose) {
+      print('[VERBOSE] All arguments: ${results.arguments}');
+    }
+    final port = int.tryParse(results['port'] as String) ?? 8080;
+    await DartLogServer().start(port: port);
+  } on FormatException catch (e) {
+    // Print usage information if an invalid argument was provided.
+    print(e.message);
+    print('');
+    printUsage(argParser);
+  }
+}
